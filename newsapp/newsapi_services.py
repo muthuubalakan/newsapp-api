82b32eef7a492bd2c3ee33d01c5b1bc17fb3c59d
muthuubalakan <muthuumuthu@gmail.com>
2019-12-06 10:48:51 +0100

    remove old files

--- newsapp/newsapi_services.py
+++ newsapp/newsapi_services.py
@@ -16,28 +16,28 @@ class NEWSAPIService:
         self.countries = ["us", "de"]
         self.topics = ["technology", "bitcoin", "business"]
         self.category = None
-    
+
     def headlines(self, country):
         if self.query_string:
             return f'{API_URL}{TOP_NEWS}?country={country}&category={self.query_string}&apiKey={API_KEY}'
         else:
             return f'{API_URL}{TOP_NEWS}?country={country}&apiKey={API_KEY}'
-        
+
     def allnews(self, topic):
         if self.query_string:
             return f'{API_URL}{ALL_NEWS}?q={self.query_string}&apiKey={API_KEY}'
         else:
             return f'{API_URL}{ALL_NEWS}?q={topic}&apiKey={API_KEY}'
-    
+
     def fetch_data(self, url):
         return self.parse_json(requests.get(url).json())
-    
+
     def parse_json(self, data):
         articles = data['articles']
         for article in articles:
             if article['content']:
                 self.save_article(article)
-    
+
     def save_article(self, article):
         url = article.get('url', None)
         article.pop('source')
@@ -47,14 +47,13 @@ class NEWSAPIService:
         serializer = NewsSerializer(news, data=article)
         if serializer.is_valid():
             serializer.save()
-    
+
     def get_headlines(self):
         self.category = 'headlines'
         for country in self.countries:
             self.fetch_data(self.headlines(country))
-            
+
     def get_allnews(self):
         for topic in self.topics:
             self.category = topic
             self.fetch_data(self.allnews(topic))
-            
